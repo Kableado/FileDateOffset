da847a7d60bcec3eba551d5b1861e84a028e0037
Valeriano A.R <valen.alfonso@gmail.com>
2015-01-09 07:41:45 +0100

    Commit inicial

---
+++ FileDateOffset/FrmFileDateOffset.Designer.cs
@@ -0,0 +1,287 @@
+﻿namespace FileDateOffset
+{
+    partial class FrmFileDateOffset
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblPath = new System.Windows.Forms.Label();
+            this.txtPath = new System.Windows.Forms.TextBox();
+            this.txtFilter = new System.Windows.Forms.TextBox();
+            this.lblFilter = new System.Windows.Forms.Label();
+            this.cbFilter = new System.Windows.Forms.ComboBox();
+            this.btnTest = new System.Windows.Forms.Button();
+            this.btnGo = new System.Windows.Forms.Button();
+            this.lsbFiles = new System.Windows.Forms.ListBox();
+            this.txtYears = new System.Windows.Forms.TextBox();
+            this.txtMonths = new System.Windows.Forms.TextBox();
+            this.txtDays = new System.Windows.Forms.TextBox();
+            this.txtHours = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtMinutes = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtSeconds = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lblPath
+            // 
+            this.lblPath.AutoSize = true;
+            this.lblPath.Location = new System.Drawing.Point(12, 9);
+            this.lblPath.Name = "lblPath";
+            this.lblPath.Size = new System.Drawing.Size(30, 13);
+            this.lblPath.TabIndex = 8;
+            this.lblPath.Text = "Ruta";
+            // 
+            // txtPath
+            // 
+            this.txtPath.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtPath.Location = new System.Drawing.Point(79, 6);
+            this.txtPath.Name = "txtPath";
+            this.txtPath.Size = new System.Drawing.Size(235, 20);
+            this.txtPath.TabIndex = 7;
+            // 
+            // txtFilter
+            // 
+            this.txtFilter.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtFilter.Location = new System.Drawing.Point(79, 60);
+            this.txtFilter.Name = "txtFilter";
+            this.txtFilter.Size = new System.Drawing.Size(235, 20);
+            this.txtFilter.TabIndex = 12;
+            // 
+            // lblFilter
+            // 
+            this.lblFilter.AutoSize = true;
+            this.lblFilter.Location = new System.Drawing.Point(12, 35);
+            this.lblFilter.Name = "lblFilter";
+            this.lblFilter.Size = new System.Drawing.Size(29, 13);
+            this.lblFilter.TabIndex = 11;
+            this.lblFilter.Text = "Filtro";
+            // 
+            // cbFilter
+            // 
+            this.cbFilter.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.cbFilter.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cbFilter.FormattingEnabled = true;
+            this.cbFilter.Location = new System.Drawing.Point(79, 32);
+            this.cbFilter.Name = "cbFilter";
+            this.cbFilter.Size = new System.Drawing.Size(235, 21);
+            this.cbFilter.TabIndex = 10;
+            this.cbFilter.SelectedIndexChanged += new System.EventHandler(this.cbFilter_SelectedIndexChanged);
+            // 
+            // btnTest
+            // 
+            this.btnTest.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnTest.Location = new System.Drawing.Point(216, 293);
+            this.btnTest.Name = "btnTest";
+            this.btnTest.Size = new System.Drawing.Size(44, 23);
+            this.btnTest.TabIndex = 15;
+            this.btnTest.Text = "Test";
+            this.btnTest.UseVisualStyleBackColor = true;
+            this.btnTest.Click += new System.EventHandler(this.btnTest_Click);
+            // 
+            // btnGo
+            // 
+            this.btnGo.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnGo.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnGo.Location = new System.Drawing.Point(266, 287);
+            this.btnGo.Name = "btnGo";
+            this.btnGo.Size = new System.Drawing.Size(48, 32);
+            this.btnGo.TabIndex = 14;
+            this.btnGo.Text = "Go";
+            this.btnGo.UseVisualStyleBackColor = true;
+            this.btnGo.Click += new System.EventHandler(this.btnGo_Click);
+            // 
+            // lsbFiles
+            // 
+            this.lsbFiles.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.lsbFiles.FormattingEnabled = true;
+            this.lsbFiles.Location = new System.Drawing.Point(15, 118);
+            this.lsbFiles.Name = "lsbFiles";
+            this.lsbFiles.Size = new System.Drawing.Size(299, 160);
+            this.lsbFiles.TabIndex = 13;
+            // 
+            // txtYears
+            // 
+            this.txtYears.Location = new System.Drawing.Point(79, 86);
+            this.txtYears.Name = "txtYears";
+            this.txtYears.Size = new System.Drawing.Size(20, 20);
+            this.txtYears.TabIndex = 16;
+            // 
+            // txtMonths
+            // 
+            this.txtMonths.Location = new System.Drawing.Point(123, 86);
+            this.txtMonths.Name = "txtMonths";
+            this.txtMonths.Size = new System.Drawing.Size(20, 20);
+            this.txtMonths.TabIndex = 17;
+            // 
+            // txtDays
+            // 
+            this.txtDays.Location = new System.Drawing.Point(167, 86);
+            this.txtDays.Name = "txtDays";
+            this.txtDays.Size = new System.Drawing.Size(20, 20);
+            this.txtDays.TabIndex = 18;
+            // 
+            // txtHours
+            // 
+            this.txtHours.Location = new System.Drawing.Point(209, 86);
+            this.txtHours.Name = "txtHours";
+            this.txtHours.Size = new System.Drawing.Size(20, 20);
+            this.txtHours.TabIndex = 19;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(105, 88);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(12, 13);
+            this.label1.TabIndex = 20;
+            this.label1.Text = "/";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(149, 88);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(12, 13);
+            this.label2.TabIndex = 21;
+            this.label2.Text = "/";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(235, 89);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(10, 13);
+            this.label3.TabIndex = 22;
+            this.label3.Text = ":";
+            // 
+            // txtMinutes
+            // 
+            this.txtMinutes.Location = new System.Drawing.Point(251, 86);
+            this.txtMinutes.Name = "txtMinutes";
+            this.txtMinutes.Size = new System.Drawing.Size(20, 20);
+            this.txtMinutes.TabIndex = 23;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(277, 88);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(10, 13);
+            this.label4.TabIndex = 24;
+            this.label4.Text = ":";
+            // 
+            // txtSeconds
+            // 
+            this.txtSeconds.Location = new System.Drawing.Point(293, 86);
+            this.txtSeconds.Name = "txtSeconds";
+            this.txtSeconds.Size = new System.Drawing.Size(20, 20);
+            this.txtSeconds.TabIndex = 25;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(193, 89);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(10, 13);
+            this.label5.TabIndex = 26;
+            this.label5.Text = "-";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(15, 89);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(58, 13);
+            this.label6.TabIndex = 27;
+            this.label6.Text = "OffsetDate";
+            // 
+            // FrmFileDateOffset
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(326, 331);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.txtSeconds);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtMinutes);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtHours);
+            this.Controls.Add(this.txtDays);
+            this.Controls.Add(this.txtMonths);
+            this.Controls.Add(this.txtYears);
+            this.Controls.Add(this.btnTest);
+            this.Controls.Add(this.btnGo);
+            this.Controls.Add(this.lsbFiles);
+            this.Controls.Add(this.txtFilter);
+            this.Controls.Add(this.lblFilter);
+            this.Controls.Add(this.cbFilter);
+            this.Controls.Add(this.lblPath);
+            this.Controls.Add(this.txtPath);
+            this.MinimumSize = new System.Drawing.Size(342, 370);
+            this.Name = "FrmFileDateOffset";
+            this.Text = "FileDateOffset";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblPath;
+        private System.Windows.Forms.TextBox txtPath;
+        private System.Windows.Forms.TextBox txtFilter;
+        private System.Windows.Forms.Label lblFilter;
+        private System.Windows.Forms.ComboBox cbFilter;
+        private System.Windows.Forms.Button btnTest;
+        private System.Windows.Forms.Button btnGo;
+        private System.Windows.Forms.ListBox lsbFiles;
+        private System.Windows.Forms.TextBox txtYears;
+        private System.Windows.Forms.TextBox txtMonths;
+        private System.Windows.Forms.TextBox txtDays;
+        private System.Windows.Forms.TextBox txtHours;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtMinutes;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtSeconds;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+    }
+}
---
+++ FileDateOffset/FrmFileDateOffset.cs
@@ -0,0 +1,303 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace FileDateOffset
+{
+    public partial class FrmFileDateOffset : Form
+    {
+        public string InitialPath
+        {
+            get { return txtPath.Text; }
+            set { txtPath.Text = value; }
+        }
+
+        public FrmFileDateOffset()
+        {
+            InitializeComponent();
+            txtPath.Text = Directory.GetCurrentDirectory();
+            cbFilter_Init();
+        }
+
+        #region Filter
+
+        private void cbFilter_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            txtFilter.Enabled = (cbFilter.SelectedIndex == 4);
+        }
+
+        private void cbFilter_Init()
+        {
+            this.cbFilter.Items.AddRange(new object[] {
+                "All",
+                "DSC_ + MOV_",
+                "IMG_ + VID_",
+                "DSC_ + MOV_ + IMG_ + VID_",
+                "Custom"});
+            cbFilter.SelectedIndex = 0;
+            txtFilter.Enabled = false;
+        }
+
+        private bool cbFilter_ApplyFilter(string fileName)
+        {
+            switch (cbFilter.SelectedIndex)
+            {
+                case 0:
+                    return true;
+                case 1:
+                    return (fileName.StartsWith("DSC_") || fileName.StartsWith("MOV_"));
+                case 2:
+                    return (fileName.StartsWith("IMG_") || fileName.StartsWith("VID_"));
+                case 3:
+                    return (fileName.StartsWith("DSC_") || fileName.StartsWith("IMG_") || fileName.StartsWith("MOV_") || fileName.StartsWith("VID_"));
+                case 4:
+                    return txtFilter_ApplyFilter(fileName);
+            }
+            return false;
+        }
+
+        private bool txtFilter_ApplyFilter(string fileName)
+        {
+            if (string.IsNullOrEmpty(txtFilter.Text))
+            {
+                return false;
+            }
+
+            string[] filters = txtFilter.Text.Split('|');
+
+            foreach (string strFilter in filters)
+            {
+                if (fileName.Contains(strFilter))
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        #endregion
+
+        #region lsbFiles
+
+        private void lsbFiles_Clean()
+        {
+            lsbFiles.Items.Clear();
+        }
+
+        private void lsbFiles_AddLine(string line)
+        {
+            lsbFiles.Items.Add(line);
+
+            Application.DoEvents();
+
+            int visibleItems = lsbFiles.ClientSize.Height / lsbFiles.ItemHeight;
+            lsbFiles.TopIndex = Math.Max(lsbFiles.Items.Count - visibleItems + 1, 0);
+        }
+
+        #endregion
+
+        #region CreationDate
+
+        /// <summary>
+        /// Returns the EXIF Image Data of the Date Taken.
+        /// </summary>
+        /// <param name="getImage">Image (If based on a file use Image.FromFile(f);)</param>
+        /// <returns>Date Taken or Null if Unavailable</returns>
+        public static DateTime? GetExifDateTaken(Image image)
+        {
+            int DateTakenValue = 0x9003; //36867;
+
+            if (!image.PropertyIdList.Contains(DateTakenValue))
+                return null;
+
+            string dateTakenTag = Encoding.ASCII.GetString(image.GetPropertyItem(DateTakenValue).Value);
+            string[] parts = dateTakenTag.Split(':', ' ');
+            int year = int.Parse(parts[0]);
+            int month = int.Parse(parts[1]);
+            int day = int.Parse(parts[2]);
+            int hour = int.Parse(parts[3]);
+            int minute = int.Parse(parts[4]);
+            int second = int.Parse(parts[5]);
+
+            return new DateTime(year, month, day, hour, minute, second);
+        }
+
+        public static void SetExifDateTaken(Image image, DateTime dtTaken)
+        {
+            int DateTakenValue = 0x9003; //36867;
+
+            if (!image.PropertyIdList.Contains(DateTakenValue))
+                return;
+
+            string strDateTaken = string.Format("{0}:{1}:{2} {3}:{4}:{5}\0", dtTaken.Year, dtTaken.Month, dtTaken.Day,
+                                                dtTaken.Hour, dtTaken.Minute, dtTaken.Second);
+            byte[] bytesDateTaken = Encoding.ASCII.GetBytes(strDateTaken);
+            PropertyItem prop = image.GetPropertyItem(DateTakenValue);
+            prop.Len = bytesDateTaken.Length;
+            prop.Value = bytesDateTaken;
+            image.RemovePropertyItem(DateTakenValue);
+            image.SetPropertyItem(prop);
+        }
+
+        public static DateTime GetFileCreationDate(string filePath)
+        {
+            DateTime dtFile;
+
+            DateTime dtCretation = File.GetCreationTime(filePath);
+            DateTime dtLastMod = File.GetLastWriteTime(filePath);
+            if (dtCretation < dtLastMod)
+            {
+                dtFile = dtCretation;
+            }
+            else
+            {
+                dtFile = dtLastMod;
+            }
+
+            string ext = Path.GetExtension(filePath).ToLower();
+            if (ext == ".png" || ext == ".jpg" || ext == ".jpeg" || ext == ".gif")
+            {
+                Image img = Image.FromFile(filePath);
+                DateTime? dtTaken = GetExifDateTaken(img);
+                if (dtTaken != null)
+                {
+                    dtFile = (DateTime)dtTaken;
+                }
+                img.Dispose();
+            }
+
+            return dtFile;
+        }
+
+        public static void SetFileCreationDate(string filePath, DateTime dtCreation)
+        {
+            string ext = Path.GetExtension(filePath).ToLower();
+            if (ext == ".png" || ext == ".jpg" || ext == ".jpeg" || ext == ".gif")
+            {
+                Image img = Image.FromFile(filePath);
+                SetExifDateTaken(img, dtCreation);
+                string filePath2 = string.Format("{0}.temp.jpg", filePath);
+                img.Save(filePath2);
+                img.Dispose();
+                File.Delete(filePath);
+                File.Move(filePath2, filePath);
+            }
+
+            File.SetCreationTime(filePath, dtCreation);
+            File.SetLastWriteTime(filePath, dtCreation);
+        }
+        
+        #endregion
+        
+        public class DateOffset
+        {
+            public int Years = 0;
+            public int Months = 0;
+            public int Days = 0;
+            public int Hours = 0;
+            public int Minutes = 0;
+            public int Seconds = 0;
+        }
+
+        public int ParseInt(string text)
+        {
+            if (!string.IsNullOrEmpty(text))
+            {
+                int temp;
+                if (int.TryParse(text, out temp))
+                {
+                    return temp;
+                }
+            }
+            return 0;
+        }
+
+        public DateOffset GetDateOffset()
+        {
+            var offset = new DateOffset
+                {
+                    Years = ParseInt(txtYears.Text),
+                    Months = ParseInt(txtMonths.Text),
+                    Days = ParseInt(txtDays.Text),
+                    Hours = ParseInt(txtHours.Text),
+                    Minutes = ParseInt(txtMinutes.Text),
+                    Seconds = ParseInt(txtSeconds.Text)
+                };
+            return offset;
+        }
+
+        private void ProcessFile(string filePath, string fileName, bool doAction, DateOffset dateOffset)
+        {
+            DateTime dtFile = GetFileCreationDate(filePath);
+            if (doAction)
+            {
+                dtFile =
+                    dtFile.AddSeconds(dateOffset.Seconds)
+                          .AddMinutes(dateOffset.Minutes)
+                          .AddHours(dateOffset.Hours)
+                          .AddDays(dateOffset.Days)
+                          .AddMonths(dateOffset.Months)
+                          .AddYears(dateOffset.Years);
+                SetFileCreationDate(filePath, dtFile);
+            }
+
+            lsbFiles_AddLine(string.Format("ApplyOffset: {0} {1} ", fileName, dtFile.ToString()));
+        }
+
+        private void ProcessDirectory(string path, bool doAction, DateOffset dateOffset)
+        {
+            string[] filePaths = Directory.GetFiles(path);
+            foreach (string filePath in filePaths)
+            {
+                string fileName = Path.GetFileName(filePath);
+
+                if (cbFilter_ApplyFilter(fileName))
+                {
+                    ProcessFile(filePath, fileName, doAction, dateOffset);
+                }
+                else
+                {
+                    lsbFiles_AddLine(string.Format("Unchanged: {0}", fileName));
+                }
+            }
+
+            string[] directoryPaths = Directory.GetDirectories(path);
+            foreach (string directoryPath in directoryPaths)
+            {
+                ProcessDirectory(directoryPath, doAction, dateOffset);
+            }
+        }
+
+        private void Process(bool doAction)
+        {
+            string path = txtPath.Text;
+            if (!Directory.Exists(path))
+            {
+                MessageBox.Show("El directorio no existe");
+                return;
+            }
+
+            lsbFiles_Clean();
+            DateOffset dateOffset = GetDateOffset();
+            ProcessDirectory(path, doAction, dateOffset);
+        }
+
+        private void btnGo_Click(object sender, EventArgs e)
+        {
+            Process(true);
+        }
+
+        private void btnTest_Click(object sender, EventArgs e)
+        {
+            Process(false);
+        }
+
+    }
+}
